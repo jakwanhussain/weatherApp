29a3a1b4e23fc5b578659642c884baac815abb62
Jakwan <jakwanhussain@gmail.com>
2017-02-17 22:26:34 +0000

    Should work
    Should work

--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -23,10 +23,9 @@ export default class Iphone extends Component {
 	// a call to fetch weather data via wunderground
 	fetchWeatherData = () => {
 		// API URL with a structure of : ttp://api.wunderground.com/api/key/feature/q/country-code/city.json
-		var url = "http://api.wunderground.com/api/dbd7f2b02a0f874b/conditions/q/FRANCE/Paris.json";
-		//var url = "http://api.wunderground.com/api/dbd7f2b02a0f874b/conditions/q/NEPAL/kathmandu.json";
+		var url = "http://api.wunderground.com/api/c78f1a13d2ca6971/conditions/q/UK/London.json";
 		$.ajax({
-			url: url
+			url: url,
 			dataType: "jsonp",
 			success : this.parseResponse,
 			error : function(req, err){ console.log('API call failed ' + err); }
